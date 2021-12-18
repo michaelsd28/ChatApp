b24d30f79c7315e960995dc7c770d40e11a27adb
Michael <santana11281@gmail.com>
2021-12-18 03:18:35 -0400

    ws working

--- src/Components/Message/WebSocketClient.js
+++ src/Components/Message/WebSocketClient.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-
+import { setInterval } from "stompjs";
 // import SockJS from "sockjs-client";
 // import socketIOClient from "socket.io-client";
 
@@ -8,39 +7,75 @@ function WebSocketClient() {
   /* connect stomp client */
   const [messages, setMessages] = React.useState([]);
   const [connected, setConnected] = React.useState(false);
-  const [connection, setConnection] = React.useState(
-    new WebSocket("ws://localhost:8080/multiple")
-  );
-
-  // This can also be an async getter function. See notes below on Async Urls.
+  const [connection, setConnection] = React.useState(null);
 
 
 
+
+
+  function connectionWS() {
+
+ 
+    if (connection == null) {
+      setConnection(new WebSocket("ws://localhost:8080/ws"));
+      setConnected(true);
+    } 
+
+    if ( connection !== null ) {
+    connection.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+
+    connection.onmessage = evt => {
+      console.log("Message Received: " + evt.data);
+      setMessages(messages => [...messages, evt.data]);
+    };
+
+    connection.onclose = () => {
+      console.log("WebSocket Client Disconnected");
+      connection.close();
+      setConnection(null);
+      connectionWS();
+    };
+
+
+  }
+    return connection;
+  }
+
+
+
+
+  connectionWS();
 
   React.useEffect(() => {
+
+
+    console.log("open connection -");
+
+    if (connected) {
+      connection.addEventListener("message", function (event) {
+        console.log("Message from server ", event.data);
+
+        setMessages((messages) => [...messages, event.data]);
+      });
+    }
+
     // socket.close()
     // console.log("close connection -");
-  }, []);
+  }, [setConnection]);
 
   return (
     <div style={{ padding: 50 }}>
       <button
         className="btn-danger"
         onClick={() => {
-          
-          const connection = new WebSocket("ws://localhost:8080/multiple");
-          connection.onopen = () => {
-            console.log("open connection");
-          };
-          connection.onmessage = event => {
-            console.log("message", event.data);
-            setMessages(messages => [...messages, event.data]);
-          };
-        
-    
+          // connectionWS().send("Hello");
+          connection.send("hi");
 
-          // clear messages
-          // setMessages([]);
+          setInterval(() => {
+            connection.close();
+          }, 1000);
         }}
       >
         send message
