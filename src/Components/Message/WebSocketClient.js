5682dab433bd43630d1a6b94332008719f9e0275
Michael <santana11281@gmail.com>
2021-12-19 03:48:06 -0400

    refresh not working  12/19/2021

--- src/Components/Message/WebSocketClient.js
+++ src/Components/Message/WebSocketClient.js
@@ -1,81 +1,74 @@
 import React from "react";
-import { setInterval } from "stompjs";
-// import SockJS from "sockjs-client";
-// import socketIOClient from "socket.io-client";
 
 function WebSocketClient() {
   /* connect stomp client */
   const [messages, setMessages] = React.useState([]);
   const [connected, setConnected] = React.useState(false);
   const [connection, setConnection] = React.useState(null);
-
-
-
-
+  const [sessionId, setSessionId] = React.useState(null);
 
   function connectionWS() {
-
- 
     if (connection == null) {
       setConnection(new WebSocket("ws://localhost:8080/ws"));
       setConnected(true);
-    } 
+    }
 
-    if ( connection !== null ) {
-    connection.onopen = () => {
-      console.log("WebSocket Client Connected");
-    };
+    if (connection !== null) {
+      connection.onopen = () => {
+        console.log("WebSocket Client Connected");
+        setConnected(true);
+      };
 
-    connection.onmessage = evt => {
-      console.log("Message Received: " + evt.data);
-      setMessages(messages => [...messages, evt.data]);
-    };
+      connection.onmessage = (evt) => {
+        if (evt.data.includes("Welcome to the server ")) {
+          console.log("Message Received: " + evt.data);
+          setMessages([...messages, evt.data]);
+          let id = evt.data.replace("Welcome to the server ", "");
+          setSessionId(id);
+        } else {
+          console.log("Message Received: " + evt.data);
+          setMessages((messages) => [...messages, evt.data]);
+        }
 
-    connection.onclose = () => {
-      console.log("WebSocket Client Disconnected");
-      connection.close();
-      setConnection(null);
-      connectionWS();
-    };
+        console.log("your session id is: " + sessionId);
+      };
 
-
-  }
+      connection.onclose = () => {
+        console.log("WebSocket Client Disconnected");
+        connectionWS();
+      };
+    }
     return connection;
   }
 
+  function isOpen(ws) { return ws.readyState === ws.OPEN }
 
-
-
+  if (!connected) {
+    connectionWS();
+  }
   connectionWS();
 
   React.useEffect(() => {
-
-
     console.log("open connection -");
-
-    if (connected) {
-      connection.addEventListener("message", function (event) {
-        console.log("Message from server ", event.data);
-
-        setMessages((messages) => [...messages, event.data]);
-      });
-    }
-
-    // socket.close()
     // console.log("close connection -");
-  }, [setConnection]);
+  }, []);
+
+
+
 
   return (
     <div style={{ padding: 50 }}>
       <button
         className="btn-danger"
         onClick={() => {
-          // connectionWS().send("Hello");
-          connection.send("hi");
+    
+          if (isOpen(connection)) {
+            connection.send(sessionId+ " Hello from the client");
+          }  
 
-          setInterval(() => {
-            connection.close();
-          }, 1000);
+          if(!isOpen(connection)) return;
+
+       
         }}
       >
         send message
