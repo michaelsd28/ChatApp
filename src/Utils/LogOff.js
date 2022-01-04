ad1cfcdec7b65ba3f5fe25df2ef7c545c34b567a
Michael <santana11281@gmail.com>
2022-01-04 00:33:38 -0400

    post working

--- src/Utils/LogOff.js
+++ src/Utils/LogOff.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function LogOff() {
-    return (
-        <div>
-            <button onClick={() => {
-                localStorage.removeItem("jwt");
-                window.location.reload();
-            }}
-            className='btn btn-outline-danger'>
-               <img 
-            style={{
-             
-                width: "25px",
-                padding:"5px"
-            }}
-            src='images/logoff.png' alt='logoff'/> Log Off</button>
-            
-            
-        </div>
-    )
+  return (
+    <div>
+      <button
+        onClick={() => {
+          localStorage.removeItem("jwt");
+          window.location.reload();
+        }}
+        className="btn btn-outline-danger"
+      >
+        <img
+          style={{
+            width: "25px",
+            padding: "5px"
+          }}
+          src={process.env.PUBLIC_URL + "/images/logoff.png"}
+        />
+        Log Off
+      </button>
+    </div>
+  );
 }
 
-export default LogOff
+export default LogOff;
